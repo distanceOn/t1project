b5279b69b782d66e4d6dd75767c0a0b6b060857e
Roman Kondrashev <distanceOn@yandex.ru>
2024-02-11 16:21:32 +0300

    feat: finish products list and style it

--- src/components/organisms/Products/Products.tsx
+++ src/components/organisms/Products/Products.tsx
@@ -4,8 +4,10 @@ import S from './Products.module.scss';
 export const Products = () => {
   return (
     <div className={S.container}>
-      <ul>
-        <ProductCard />
+      <ul className={S.products}>
+        {Array.from({ length: 9 }).map((_, index) => (
+          <ProductCard key={index} />
+        ))}
       </ul>
     </div>
   );
