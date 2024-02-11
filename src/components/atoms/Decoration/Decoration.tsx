b083312babfd6d63277522bf1479377f33f9c17e
Roman Kondrashev <distanceOn@yandex.ru>
2024-02-11 18:37:48 +0300

    feat: create ellipsedecoration and style it for template

---
+++ src/components/atoms/Decoration/Decoration.tsx
@@ -0,0 +1,10 @@
+import S from './Decoration.module.scss';
+
+type DecorationProps = {
+  className: string;
+};
+
+export const Decoration = ({ className }: DecorationProps) => {
+  const totalClass = S.decoration + ' ' + className;
+  return <div className={totalClass}></div>;
+};
