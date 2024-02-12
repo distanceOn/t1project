f26b929c7523c40c44745ee3e0a9b33043d207c4
Roman Kondrashev <distanceOn@yandex.ru>
2024-02-12 22:54:14 +0300

    feat: create styles for teammate and finish teammate molecule

--- src/components/molecules/Teammate/Teammate.tsx
+++ src/components/molecules/Teammate/Teammate.tsx
@@ -1,4 +1,5 @@
 import { Picture } from '../../atoms/Picture/Picture';
+import { Title } from '../../atoms/Title/Title';
 import S from './Teammate.module.scss';
 
 type TeammateProps = {
@@ -8,6 +9,14 @@ export const Teammate = ({ image }: TeammateProps) => {
   return (
     <div className={S.container}>
       <Picture size='large' image={image} />
+      <div className={S.info}>
+        <Title color='white' size='small'>
+          Maxim
+        </Title>
+        <Title color='white' size='min'>
+          Administrator
+        </Title>
+      </div>
     </div>
   );
 };
