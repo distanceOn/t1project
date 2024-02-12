b96b1a761151c0959fe22023b9e399b3ed8831d3
Roman Kondrashev <distanceOn@yandex.ru>
2024-02-13 02:14:36 +0300

    feat: finish creation of faq

--- src/components/organisms/Faq/Faq.tsx
+++ src/components/organisms/Faq/Faq.tsx
@@ -1,9 +1,16 @@
+import { Title } from '../../atoms/Title/Title';
 import { FaqItem } from '../../molecules/FaqItem/FaqItem';
 import S from './Faq.module.scss';
 
 export const Faq = () => {
   return (
     <div className={S.container}>
+      <div className={S.title}>
+        <Title color='grey' size='default'>
+          FAQ
+        </Title>
+      </div>
+
       <FaqItem
         question='Question 1'
         answer='Long answer to the first question'
