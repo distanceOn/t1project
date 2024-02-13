15bda041bf9329f0955f87104266bffe8c4318e8
Roman Kondrashev <distanceOn@yandex.ru>
2024-02-13 17:33:31 +0300

    refactor: optimize images by lazy-load

--- src/components/atoms/Picture/Picture.tsx
+++ src/components/atoms/Picture/Picture.tsx
@@ -1,9 +1,13 @@
 import { PictureProps } from './types';
 import { getPictureClassName, getSrcAndAlt } from './utils';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
 
 export const Picture = ({ size, image }: PictureProps) => {
   const className = getPictureClassName(size);
   const { src, alt } = getSrcAndAlt(image);
 
-  return <img src={src} alt={alt} className={className} />;
+  return (
+    <LazyLoadImage src={src} alt={alt} effect='blur' className={className} />
+  );
 };
