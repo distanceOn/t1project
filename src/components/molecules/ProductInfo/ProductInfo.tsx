452122d02e81983a2de904740a1ff222d7167a84
Roman Kondrashev <distanceOn@yandex.ru>
2024-02-18 20:08:48 +0300

    feat: create content for product without rating

--- src/components/molecules/ProductInfo/ProductInfo.tsx
+++ src/components/molecules/ProductInfo/ProductInfo.tsx
@@ -9,12 +9,75 @@ export const ProductInfo = () => {
         <Title size='small' color='grey'>
           Puma Force 1 Shadow
         </Title>
-        <div className={S.id}>
+        <div className={S.item}>
           <Text size='default' color='lightgrey'>
             SKU ID
           </Text>
           <Text size='default' color='grey'>
             0005
+          </Text>
+        </div>
+      </div>
+      <div className={S.info}>
+        <div className={S.item}>
+          <Text size='default' color='lightgrey'>
+            Raiting
+          </Text>
+        </div>
+        <div className={S.item}>
+          <Text size='default' color='lightgrey'>
+            Base price
+          </Text>
+          <Text size='default' color='grey'>
+            500$
+          </Text>
+        </div>
+        <div className={S.item}>
+          <Text size='default' color='lightgrey'>
+            Discount percentage
+          </Text>
+          <Text size='default' color='grey'>
+            10%
+          </Text>
+        </div>
+        <div className={S.item}>
+          <Text size='default' color='lightgrey'>
+            Discount price
+          </Text>
+          <Text size='default' color='grey'>
+            450$
+          </Text>
+        </div>
+        <div className={S.item}>
+          <Text size='default' color='lightgrey'>
+            Stock
+          </Text>
+          <Text size='default' color='grey'>
+            13
+          </Text>
+        </div>
+        <div className={S.item}>
+          <Text size='default' color='lightgrey'>
+            Brand
+          </Text>
+          <Text size='default' color='grey'>
+            Puma
+          </Text>
+        </div>
+        <div className={S.item}>
+          <Text size='default' color='lightgrey'>
+            Category
+          </Text>
+          <Text size='default' color='grey'>
+            Smartphones
+          </Text>
+        </div>
+        <div className={S.item}>
+          <Text size='default' color='lightgrey'>
+            Description
+          </Text>
+          <Text size='default' color='grey'>
+            An apple mobile which is nothing like apple
           </Text>
         </div>
       </div>
