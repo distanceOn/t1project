ca2fad9ecf549a386481bdf7c99b2526c6a69d82
Roman Kondrashev <distanceOn@yandex.ru>
2024-02-25 23:51:25 +0300

    feat: connect a lot of libs and jest for testing, create the first test for text atom

---
+++ src/components/atoms/Text/Text.test.ts
@@ -0,0 +1,25 @@
+import {
+  getTotalTextClassName,
+  getSizeClassName,
+  getColorClassName,
+} from './utils';
+
+describe('Text component tests', () => {
+  test('should return correct total class name based on color and size', () => {
+    const totalClassName = getTotalTextClassName({
+      color: 'grey',
+      size: 'default',
+    });
+    expect(totalClassName).toBe('text color_grey text_default');
+  });
+
+  test('should return correct color class name', () => {
+    const colorClassName = getColorClassName('white');
+    expect(colorClassName).toBe('color_white');
+  });
+
+  test('should return correct size class name', () => {
+    const sizeClassName = getSizeClassName('thin');
+    expect(sizeClassName).toBe('text_thin');
+  });
+});
