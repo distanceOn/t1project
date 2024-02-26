96a2c1978962df9e737f76e7ece5abdbaa43fe73
Roman Kondrashev <distanceOn@yandex.ru>
2024-02-27 01:58:38 +0300

    feat: create tests for button atom, finish refactoring and test atoms

---
+++ src/components/atoms/Button/constants.ts
@@ -0,0 +1,16 @@
+import S from './Button.module.scss';
+
+export const btnColors = {
+  primary: S.color_primary,
+  secondary: S.color_secondary,
+  transparent: S.color_transparent,
+};
+
+export const btnTypes = {
+  default: S.btn_default,
+  reset: S.btn_reset,
+  apply: S.btn_apply,
+  category: S.btn_category,
+  steps: S.btn_steps,
+  catalog: S.btn_catalog,
+};
--- src/components/atoms/HeaderFooterLink/HeaderFooterLink.tsx
+++ src/components/atoms/HeaderFooterLink/HeaderFooterLink.tsx
@@ -5,15 +5,29 @@ type HeaderLinkProps = {
   href: string;
   children: React.ReactNode;
   onClick?: (event: React.MouseEvent<HTMLAnchorElement>) => void;
+  target?: string;
+  disabled?: boolean;
 };
 
 export const HeaderFooterLink = ({
   href,
   children,
   onClick,
+  target,
+  disabled,
 }: HeaderLinkProps) => {
+  const handleClick = (event: React.MouseEvent<HTMLAnchorElement>) => {
+    if (disabled) {
+      event.preventDefault();
+      return;
+    }
+
+    if (onClick) {
+      onClick(event);
+    }
+  };
   return (
-    <Link className={S.link} to={href} onClick={onClick}>
+    <Link target={target} className={S.link} to={href} onClick={handleClick}>
       {children}
     </Link>
   );
